28ea74022df9b3229a68e7979d97197442bdcdcc
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-09-12 09:34:40 +0100

    Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,46 +1,72 @@
 #!/usr/bin/env python3
-"""Authentication module for the API.
+""" Now you will create a class to manage the API authentication.
+Create a folder api/v1/auth
+Create an empty file api/v1/auth/__init__.py
+Create the class Auth:
+in the file api/v1/auth/auth.py
+import request from flask
+class name Auth
+public method def require_auth(self, path: str, excluded_paths: List[str])
+ -> bool: that returns False - path and excluded_paths will be used later,
+ now, you don't need to take care of them
+public method def authorization_header(self, request=None) -> str: that
+returns None - request will be the Flask request object
+public method def current_user(self, request=None) -> TypeVar('User'):
+ that returns None - request will be the Flask request object
+This class is the template for all authentication system you will implement.
 """
+# from flask import request
+from typing import (
+    List, TypeVar
+)
 import os
-import re
-from typing import List, TypeVar
-from flask import request
 
 
 class Auth:
-    """Authentication class.
+    """
+    This is the Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
-        """
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
+        """public require_auth method
+            Return bool"""
+        if not path:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
                     return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == '*':
+                    if path.startswith(i[:-1]):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Gets the authorization header field from the request.
-        """
-        if request is not None:
-            return request.headers.get('Authorization', None)
-        return None
+        """Return authorization value from request header"""
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Gets the current user from the request.
-        """
+        """return user or none"""
         return None
 
-    def session_cookie(self, request=None) -> str:
-        """Gets the value of the cookie named SESSION_NAME.
-        """
-        if request is not None:
-            cookie_name = os.getenv('SESSION_NAME')
-            return request.cookies.get(cookie_name)
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request: """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        cookie = request.cookies.get(session_name)
+        return cookie
